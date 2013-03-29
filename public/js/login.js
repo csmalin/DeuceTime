00d35c8788dc4387820abe5c343b6c56ddc8aa4a
DBC Apprentice <apprentice@devbootcamp.com>
2013-03-28 20:57:20 -0700

    no more map. login works

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
+// $(document).ready(function(){
   
-  $('a.login').on('click', function(e){
-    e.preventDefault();
+//   $('a.login').on('click', function(e){
+//     e.preventDefault();
     
-    if ($('form.signin').css("visibility") == "hidden")
-      {
-        $('form.signin').css("visibility", "visible");
-      }
-    else
-      {
-        $('form.signin').css("visibility", "hidden")
-      }
-  });
-});
+//     if ($('form.signin').css("visibility") == "hidden")
+//       {
+//         $('form.signin').css("visibility", "visible");
+//       }
+//     else
+//       {
+//         $('form.signin').css("visibility", "hidden")
+//       }
+//   });
+// });
