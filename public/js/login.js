d531df40d88bae9bd0a4b68af373bede1cd67f11
Chris Malin <csmalin@ubuntu.(none)>
2013-03-28 18:32:51 -0700

    More Nav, yay

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  
+  $('a.login').on('click', function(e){
+    e.preventDefault();
+    
+    if ($('form.signin').css("visibility") == "hidden")
+      {
+        $('form.signin').css("visibility", "visible");
+      }
+    else
+      {
+        $('form.signin').css("visibility", "hidden")
+      }
+  });
+});
